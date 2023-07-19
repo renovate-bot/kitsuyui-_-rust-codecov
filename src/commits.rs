e73faedd9b5567ec559413730f86cda3dcf72efc
kitsuyui <kitsuyui@kitsuyui.com>
2023-07-19 22:51:26 +0900

    get coverage from CommitsAPIResponse

--- src/commits.rs
+++ src/commits.rs
@@ -63,3 +63,63 @@ pub struct Totals {
     pub complexity_ratio: f64,
     pub diff: usize,
 }
+
+impl CommitsAPIResponse {
+    pub fn coverage(&self) -> Option<f64> {
+        if self.count == 0 {
+            return None;
+        }
+        let mut total_coverage = 0.0;
+        for commit in &self.results {
+            total_coverage += commit.totals.coverage;
+        }
+        Some(total_coverage / self.count as f64)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_coverage() {
+        use super::*;
+        let mut response = CommitsAPIResponse {
+            results: vec![],
+            count: 1,
+            next: None,
+            previous: None,
+            total_pages: 1,
+        };
+        assert_eq!(response.coverage(), Some(0.0));
+        let commit = Commit {
+            commitid: String::from("123"),
+            message: String::from("message"),
+            timestamp: String::from("timestamp"),
+            ci_passed: true,
+            author: CommitAuthor {
+                service: String::from("service"),
+                username: String::from("username"),
+                name: Some(String::from("name")),
+            },
+            branch: Some(String::from("branch")),
+            totals: Totals {
+                files: 1,
+                lines: 1,
+                hits: 1,
+                misses: 1,
+                partials: 1,
+                coverage: 2.0,
+                branches: 1,
+                methods: 1,
+                sessions: 1,
+                complexity: 1.0,
+                complexity_total: 1.0,
+                complexity_ratio: 1.0,
+                diff: 1,
+            },
+            state: String::from("state"),
+            parent: Some(String::from("parent")),
+        };
+        response.results.push(commit);
+        assert_eq!(response.coverage(), Some(2.0));
+    }
+}
